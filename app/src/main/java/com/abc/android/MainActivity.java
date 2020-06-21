a88c8836260327f98b3e5c6e72d84bd14fd516b2
wolfhan <wolfwolfhan@gmail.com>
2020-06-21 21:48:18 +0800

    Add property animator

--- app/src/main/java/com/abc/android/MainActivity.java
+++ app/src/main/java/com/abc/android/MainActivity.java
@@ -1,14 +1,24 @@
 package com.abc.android;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView helloText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helloText = findViewById(R.id.hello_text);
+
+        AnimatorSet animatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.trans_alpha_animator);
+        animatorSet.setTarget(helloText);
+        animatorSet.start();
     }
 }
